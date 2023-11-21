51ce886a56031efc63ea45dce69d9bd1341ba67e
Fabio Carucci <garu2000@live.it>
2023-11-21 21:26:20 +0100

    Ex. 4 finito

--- S3P1-scaricabile.js
+++ S3P1-scaricabile.js
@@ -3,25 +3,68 @@
  La funzione deve ritornare la somma di quei due valori, ma se il loro valore è lo stesso allora deve ritornare la loro somma moltiplicata per 3.
 */
 
+// let numberOne = 10;
+// let numberTwo = 10;
+
+// function crazySum (int1, int2) {
+//     let sum = 0;
+//     (int1 === int2) ? sum = (int1 + int2)*3 : sum = (int1 + int2);
+//     return sum;
+// }
+
+// console.log(crazySum(numberOne, numberTwo));
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 2
  Scrivi una funzione chiamata "boundary", che accetta un numero intero come parametro e ritorna true se tale parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// let myNum = 400;
+
+// function boundary (number) {
+//     if(number > 20 && number <= 100 || number === 400) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(boundary(myNum));
 
 /* ESERCIZIO 3
  Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna invertita (es.: EPICODE => EDOCIPE).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+// let myOriginalString = "FABIOCARUCCI";
+
+// function reverseString (stringToReverse) {
+//     let myReverseString = "";
+//     for(let i = stringToReverse.length - 1; i >= 0; i--){
+//         myReverseString = myReverseString.concat(stringToReverse[i]);
+//     }
+    
+//     return myReverseString;
+// }
+
+// console.log(reverseString (myOriginalString));
 
 /* ESERCIZIO 4
  Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+let myString = "ciao mi chiamo fabio carucci e vorrei andare a surfare domani";
+
+function upperFirst (string) {
+    separateWords = string.split(" ");
+
+    for (let i = 0; i < separateWords.length; i++) {
+        separateWords[i] = separateWords[i].charAt(0).toUpperCase() + separateWords[i].slice(1);
+    }
+    return separateWords.join(" ");
+}
+
+  console.log(upperFirst(myString));
 
 /* ESERCIZIO 5
  Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
